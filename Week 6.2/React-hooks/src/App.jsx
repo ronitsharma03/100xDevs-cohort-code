d4b28f9492149198e6793e34b2382763e73052bd
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-03-30 16:27:05 +0530

    React hooks - useEffect()

--- Week 6.2/React-hooks/src/App.jsx
+++ Week 6.2/React-hooks/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Todo id={3} />
   )
 }
+
+function Todo({id}){
+  const [todos, setTodos] = useState({});
+
+  useEffect(()=>{
+    // fetch("https://sum-server.100xdevs.com/todo?id="+ id)
+    // .then(async function(response){
+    //   const json = await response.json();
+    //   setTodos(json.todo);
+    // })
+    axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+    .then(function(res){
+      setTodos(res.data.todo)
+    })
+  }, []);
+return <>
+
+  <h1>{todos.title}</h1>
+  <h2>{todos.description}</h2>
+</>
+}
+
 
 export default App
