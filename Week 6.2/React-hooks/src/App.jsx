1eca961ae4b97373515d55b59f1e143f197b7cc3
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-03-30 17:00:13 +0530

    Another example of useEffect()

--- Week 6.2/React-hooks/src/App.jsx
+++ Week 6.2/React-hooks/src/App.jsx
@@ -2,8 +2,25 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 
 function App() {
+  const [id, selectedId] = useState(1);
+  
   return (
-      <Todo id={3} />
+    <>
+      <button onClick={()=>{
+        selectedId(1)
+      }}>1</button>
+      <button onClick={()=>{
+        selectedId(2)
+      }}>2</button>
+      <button onClick={()=>{
+        selectedId(3)
+      }}>3</button>
+      <button onClick={()=>{
+        selectedId(4)
+      }}>4</button>
+
+      <Todo id={id} />
+    </>
   )
 }
 
@@ -16,11 +33,11 @@ function Todo({id}){
     //   const json = await response.json();
     //   setTodos(json.todo);
     // })
-    axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+    axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
     .then(function(res){
       setTodos(res.data.todo)
     })
-  }, []);
+  }, [id]);
 return <>
 
   <h1>{todos.title}</h1>
