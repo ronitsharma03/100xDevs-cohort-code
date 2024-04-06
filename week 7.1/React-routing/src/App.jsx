bb78cc58fb406b352e10a7cb4a9eb21e1195cc7b
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-04-06 19:10:27 +0530

    Added Routing in react

---
+++ week 7.1/React-routing/src/App.jsx
@@ -0,0 +1,36 @@
+import { Suspense, lazy, memo } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+// import { Dashboard } from './components/Dashboard'
+const Landing = lazy(() => import('./components/Landing'));
+const Dashbaord = lazy(() => import('./components/Dashboard'));
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Appbar />
+        <Routes>
+          <Route path="/dashboard" element={<Suspense fallback="Loading..."><Dashbaord /></Suspense>} />
+          <Route path="/" element={<Suspense fallback="Loading..."><Landing /></Suspense>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+const Appbar = () => {
+  const navigate = useNavigate();
+  return(
+    <>
+      <button onClick={() => {
+        navigate("/dashboard")
+      }}>Dashboard</button>
+      <button onClick={() => {
+        navigate("/")
+      }}>Home</button>
+    </>
+  )
+};
+
+export default App
