b5660035c3d0326b2b01472fe3d5038db04409fb
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-04-06 23:04:29 +0530

    Added use of Context API

--- week 7.1/Props-drilling/react-props-drilling/src/App.jsx
+++ week 7.1/Props-drilling/react-props-drilling/src/App.jsx
@@ -1,39 +1,49 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 
 import './App.css'
+import { CountContext } from './context'
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <>
-      <Count currCount={count}/>
-      <Buttons count={count} setCount={setCount}/>
+      <CountContext.Provider value={count}>
+        <Count setCount={setCount} />
+      </CountContext.Provider>
     </>
   )
 }
 
 
-function Count({currCount}) {
-  return <div style={{fontSize: "2rem", marginBottom: "10px"}}>
-    {currCount}
+function Count({ setCount }) {
+  return <div style={{ marginBottom: "10px" }}>
+    <CountRenderer />
+    <Buttons setCount={setCount} />
   </div>
+}
 
+function CountRenderer(){
+  const count = useContext(CountContext);
+  return <div style={{fontSize: "3rem"}}>
+    {count}
+  </div>
 }
 
 
-function Buttons({count, setCount}) {
-return (
-  <div style={{display: "flex", gap: "20px"}}>
-    
-    <button onClick={()=>{
-      setCount(count - 1)
-    }}>Decrease</button>
+function Buttons({ setCount }) {
+  const count = useContext(CountContext);
+  return (
+    <div style={{ display: "flex", gap: "20px" }}>
 
-    <button onClick={()=>{
-      setCount(count + 1)
-    }}>Increase</button>
-  </div>
-)
+      <button onClick={() => {
+        setCount(count - 1)
+      }}>Decrease</button>
+
+      <button onClick={() => {
+        setCount(count + 1)
+      }}>Increase</button>
+    </div>
+  )
 }
 
 export default App
