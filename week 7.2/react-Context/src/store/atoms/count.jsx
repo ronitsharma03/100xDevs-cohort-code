1bceb6c170f3643c18b12312fd5464f686178129
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-04-08 18:12:37 +0530

    Optimized use of Recoil to stop unnecessary re-renders

--- week 7.2/react-Context/src/store/atoms/count.jsx
+++ week 7.2/react-Context/src/store/atoms/count.jsx
@@ -1,6 +1,20 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const countAtom = atom({
-    key: "countAtom",
-    default: 0
+    key: "countAtom", // needs a identifier key
+    default: 0 // The initial value of the atom state as we use the useState(0);
 });
+
+// Let's use selectors 
+export const evenSelector = selector({
+    key: "evenSelector", // needs a key to identify it as the atom
+    // A key get which gives a function get
+    get: ({get}) => { // It requires a function inside which we get access to a function similar as props and destructured it
+        // for simplicity or we can do something like get: (props)=>{
+            // const count = props.get(countAtom);
+        // }
+       const count = get(countAtom); // evenSelector depends on count so we have to do get(<State dependant varioable>)
+       // Same as the dependancy array in useMemo, useCallback, useEffect
+       return count % 2;
+    }
+})
