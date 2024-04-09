d250a5c131cf57a3f65f2cbe94b286d5cf9d3dc0
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-04-09 15:37:31 +0530

    Class assignment on todo app with recoil

--- week 7.2/class assignment/todo--recoil/src/App.jsx
+++ week 7.2/class assignment/todo--recoil/src/App.jsx
@@ -1,7 +1,7 @@
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { filteredTodoListSelector } from './store/selector/selector'
 import { filterAtom, todoInputAtom, todoListAtom } from './store/atom/atoms';
-import  './index.css'
+import './index.css'
 
 const App = () => {
   const todos = useRecoilValue(filteredTodoListSelector);
@@ -16,36 +16,39 @@ const App = () => {
 
   return (
     <div className='main-container'>
-        
-        <div className='input-div'>
+
+      <div className='input-div'>
         <h1 className='h1'>Todo List</h1>
-          <input required className='placeholder-text title-input' type="text" placeholder='Title' value={todoInput.title} onChange={(e) => {
-            setTodoInput({ ...todoInput, title: e.target.value })
-          }} /> <br />
-          <textarea required className='placeholder-text text-area' rows={10} cols={20} type="text" placeholder='Description' value={todoInput.description} onChange={(e) => {
-            setTodoInput({ ...todoInput, description: e.target.value })
-          }}></textarea> <br />
-          <button type='submit' className='btn' onClick={addTodo}>Add Todo</button>
-        </div>
-        <br />
+        <input required className='placeholder-text title-input' type="text" placeholder='Title' value={todoInput.title} onChange={(e) => {
+          setTodoInput({ ...todoInput, title: e.target.value })
+        }} /> <br />
+        <textarea required className='placeholder-text text-area' rows={10} cols={20} type="text" placeholder='Description' value={todoInput.description} onChange={(e) => {
+          setTodoInput({ ...todoInput, description: e.target.value })
+        }}></textarea> <br />
+        <button type='submit' className='btn' onClick={addTodo}>Add Todo</button>
+      </div>
+      <br />
+      <div className='filter-todo'>
         <div className='filter-div'>
-          <input className='placeholder-text filter-input'type="text" placeholder='Search' value={filter} onChange={(e) => {
+          <input className='placeholder-text filter-input' type="text" placeholder='Search' value={filter} onChange={(e) => {
             setFilter(e.target.value)
           }} />
         </div>
-
         <div className='todo-div'>
-          <ul className='list'>
-            {
-              todos.map((item, index) => 
-                <li className='list-item' key={index}>
-                  <strong>{item.title}</strong> 
-                  {item.description}
-                </li>
-              )
-            }
-          </ul>
+          {/* <ol className='list'> */}
+          {
+            todos.map((item, index) =>
+              <li className='list-item' key={index}>
+                <strong className='title'>{index + 1}. {item.title}</strong> <br />
+                <p className='desc'>{item.description}</p>
+              </li>
+            )
+          }
+          {/* </ol> */}
         </div>
+      </div>
+
+
     </div>
 
   )
