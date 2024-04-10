366c2c236702437097d056395e6d811b3a4bfb5a
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-04-10 18:11:03 +0530

    Advance recoil with backend

--- week 7.3/advanced-recoil/src/atoms.js
+++ week 7.3/advanced-recoil/src/atoms.js
@@ -1,22 +1,31 @@
 import { atom } from "recoil";
 
-export const networkAtom = atom({
-    key: "networkAtom",
-    default: 178
-});
+// export const networkAtom = atom({
+//     key: "networkAtom",
+//     default: 178
+// });
+
+// export const notificationAtom = atom({
+//     key: "notificationAtom",
+//     default: 44
+// });
+
+// export const jobsAtom = atom({
+//     key: "jobsAtom",
+//     default: 12
+// });
+
+// export const messagesAtom = atom({
+//     key: "messagesAtom",
+//     default: 0
+// });
 
 export const notificationAtom = atom({
     key: "notificationAtom",
-    default: 44
+    default: {
+        network: 0,
+        notification: 0,
+        jobs: 0,
+        messages: 0
+    }
 });
-
-export const jobsAtom = atom({
-    key: "jobsAtom",
-    default: 12
-});
-
-export const messagesAtom = atom({
-    key: "messagesAtom",
-    default: 0
-});
-
