3af7a155c3c5ed9ae1c8448f2ea03894001f29d3
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-04-10 16:11:12 +0530

    minor fixes

--- week 7.3/advanced-recoil/src/App.jsx
+++ week 7.3/advanced-recoil/src/App.jsx
@@ -1,15 +1,22 @@
 
-import { useRecoilValue } from 'recoil'
+import { useRecoilValue, RecoilRoot } from 'recoil'
 import { jobsAtom, messagesAtom, networkAtom, notificationAtom } from './atoms'
 
 function App() {
+  return (
+    <RecoilRoot>
+      <Buttons />
+    </RecoilRoot>
+  )
+}
+
+function Buttons(){
   const network = useRecoilValue(networkAtom);
   const messages = useRecoilValue(messagesAtom);
   const notifications = useRecoilValue(notificationAtom);
   const jobs = useRecoilValue(jobsAtom);
 
   return (
-    
     <>
       <button>Home</button>
       <button>My network ({network >= 100 ? "99+" : network})</button>
