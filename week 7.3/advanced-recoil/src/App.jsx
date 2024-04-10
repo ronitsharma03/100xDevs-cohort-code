4e99a8206fb857ded513e1fb537d29ca4e417476
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-04-10 16:06:46 +0530

    Added advanced recoil

---
+++ week 7.3/advanced-recoil/src/App.jsx
@@ -0,0 +1,24 @@
+
+import { useRecoilValue } from 'recoil'
+import { jobsAtom, messagesAtom, networkAtom, notificationAtom } from './atoms'
+
+function App() {
+  const network = useRecoilValue(networkAtom);
+  const messages = useRecoilValue(messagesAtom);
+  const notifications = useRecoilValue(notificationAtom);
+  const jobs = useRecoilValue(jobsAtom);
+
+  return (
+    
+    <>
+      <button>Home</button>
+      <button>My network ({network >= 100 ? "99+" : network})</button>
+      <button>Jobs ({jobs})</button>
+      <button>Notification ({notifications})</button>
+      <button>Messaging({messages})</button>
+      <button>Me</button>
+    </>
+  )
+}
+
+export default App
---
+++ week 7.3/advanced-recoil/src/atoms.js
@@ -0,0 +1,22 @@
+import { atom } from "recoil";
+
+export const networkAtom = atom({
+    key: "networkAtom",
+    default: 178
+});
+
+export const notificationAtom = atom({
+    key: "notificationAtom",
+    default: 44
+});
+
+export const jobsAtom = atom({
+    key: "jobsAtom",
+    default: 12
+});
+
+export const messagesAtom = atom({
+    key: "messagesAtom",
+    default: 0
+});
+
