28e94c7d1ca60960e9d4d9c2b525780bfd574234
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-04-12 22:40:12 +0530

    Appbar fixed

--- week 8.1/youtube-clone/src/components/Appbar.tsx
+++ week 8.1/youtube-clone/src/components/Appbar.tsx
@@ -1,8 +1,15 @@
 import { Searchbar } from "./Searchbar"
 
 export const Appbar = () => {
-    return <div className="flex justify-between pt-2 p-3">
-        <div className="inline-flex items-center">
+    return <div className="flex justify-between pt-5 p-3 pb-5 sticky top-0 bg-black">
+        <div className="inline-flex items-center text-2xl font-medium tracking-tighter">
+            <span className="ml-7 mr-2 [&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#ff0000]">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                    <path
+                        d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
+                </svg>
+            </span>
+
             Youtube
         </div>
         <div>
