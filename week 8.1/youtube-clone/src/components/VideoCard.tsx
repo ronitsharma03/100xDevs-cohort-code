1cd31b45c59a8293d0c8bdda93259d16b8c88988
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-04-12 22:08:08 +0530

    Appbar added

--- week 8.1/youtube-clone/src/components/VideoCard.tsx
+++ week 8.1/youtube-clone/src/components/VideoCard.tsx
@@ -1,8 +1,8 @@
 export function VideoCard(props: any) {
     return <div className="p-5">
         
-            <img className="rounded-xl w-full" src={props.thumbImage} alt="" />
-            <div className="grid grid-cols-12 pt-5">
+            <img className="rounded-xl w-full cursor-pointer" src={props.thumbImage} alt="" />
+            <div className="grid grid-cols-12 pt-5 cursor-pointer">
                 <div className="col-span-1">
                     <img className="rounded-full" src={props.profileImage} alt="" />
                 </div>
