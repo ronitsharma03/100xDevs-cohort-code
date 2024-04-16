decfc3e99804ab44560ebf71690ae70c62fb1014
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-04-16 19:23:15 +0530

    Added transaction table

--- week 8.2/dukaan-tailwind/src/App.jsx
+++ week 8.2/dukaan-tailwind/src/App.jsx
@@ -1,11 +1,13 @@
 import { Cards } from "./components/Cards"
 import { Appbar } from "./components/Appbar"
+import { Table } from "./components/Table"
 
 function App() {
   return (
     <div>
       <Appbar />
       <Cards />
+      <Table />
     </div>
   )
 }
