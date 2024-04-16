365538d436eb495a64f8cb409d569263215799b4
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-04-16 20:37:44 +0530

    Added footer

--- week 8.2/dukaan-tailwind/src/App.jsx
+++ week 8.2/dukaan-tailwind/src/App.jsx
@@ -1,13 +1,15 @@
 import { Cards } from "./components/Cards"
 import { Appbar } from "./components/Appbar"
 import { Table } from "./components/Table"
+import { Footer } from "./components/footer"
 
 function App() {
   return (
     <div>
       <Appbar />
       <Cards />
-      <Table status="successfull"/>
+      <Table />
+      <Footer />
     </div>
   )
 }
