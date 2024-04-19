cc1c429d9711fd24f88a72fef7fc7117edb3d479
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-04-19 18:04:55 +0530

    Implemented password hashing for signup route

--- week 8.3/paytm-main/backend/routes/user.js
+++ week 8.3/paytm-main/backend/routes/user.js
@@ -8,7 +8,6 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 
-
 // Signup route for user
 const signupBody = zod.object({
     username: zod.string().email(),
@@ -20,37 +19,48 @@ const signupBody = zod.object({
 router.post("/signup", async (req, res) => {
     const { success } = signupBody.safeParse(req.body);
     if (!success) {
+        console.log("hello")
         return res.status(411).json({
-            message: "E-mail already taken / Incorrect inputs"
+            message: "Wrong inputs"
         });
     }
 
-    const existingUser = await User.findOne({
-        username: req.body.username
-    });
-    if (existingUser) {
-        return res.status(411).json({
-            message: "Email already taken!"
-        })
+    try {
+        const existingUser = await User.findOne({
+            username: req.body.username
+        });
+
+        if (existingUser) {
+            return res.status(411).json({
+                message: "Email already taken!"
+            })
+        }
+
+        const hash = await bcrypt.hash(req.body.password, saltRounds);
+        const newUser = await User.create({
+            username: req.body.username,
+            password: hash,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName
+        });
+
+
+        const userId = newUser._id;
+        const token = jwt.sign({
+            userId
+        }, JWT_SECRET);
+
+        return res.status(200).json({
+            message: "User created successfully!",
+            token: token
+        });
     }
-    
-    const newUser = await User.create({
-        username: req.body.username,
-        password: req.body.password,
-        firstName: req.body.password,
-        lastName: req.body.lastName
-    });
-
-    const userId = newUser._id;
-
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET);
-
-    return res.status(200).json({
-        message: "User created successfully!",
-        token: token
-    });
+    catch (error) {
+        console.error("Error singing up the user", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+    }
 })
 
 
@@ -64,7 +74,7 @@ const signinBody = zod.object({
 router.post("/signin", async (req, res) => {
     const { success } = signinBody.safeParse(req.body);
 
-    if(!success){
+    if (!success) {
         return res.status(411).json({
             message: "Wrong inputs"
         });
@@ -74,8 +84,9 @@ router.post("/signin", async (req, res) => {
         username: req.body.username,
         password: req.body.password
     });
+    // console.log(user);
 
-    if(!user){
+    if (!user) {
         return res.status(411).json({
             message: "Error while logging in"
         });
