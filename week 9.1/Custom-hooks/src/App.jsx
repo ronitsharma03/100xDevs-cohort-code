0960b4abd7ba40883a96fd18bfe33fb285c9110a
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-05-01 16:50:37 +0530

    Inbuilt methods in class components - mount and unmount

--- week 9.1/Custom-hooks/src/App.jsx
+++ week 9.1/Custom-hooks/src/App.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Component } from 'react';
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
+function App() {
+  const [render, setRender] = useState(true);
+
+  useEffect(() => {
+    setInterval(() => {
+      // setRender(r => !r);
+    }, 3000);
+  }, []);
+
+  return (
+    <>
+      {render ? <MyComponent /> : <div>Component 2</div>}
+    </>
+  )
+}
+class MyComponent2 extends Component{
+  
+  // Inbuilt functions in class components
+
+  // Function that runs when the component mounts
+  componentDidMount(){
+    console.log("Component 2 mounted!");
+  }
+
+  // Function that runs when the component unmounts
+  componentWillUnmount(){
+    console.log("Component unmounted!");
+  }
+
+  render() {
+    return (
+      <div>
+        Hi using class component!
+      </div>
+    )
+  }
+}
 // function App() {
 //   const [count, setCount] = useState(0)
 
@@ -20,24 +57,43 @@ import './App.css'
 //   )
 // }
 
-class MyComponent extends Component{
-  constructor(props){
-    super(props);
-    this.state = {count: 0}
-  }
+// class MyComponent extends Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {count: 0}
+//   }
 
-  incrementCount = () => {
-    this.setState({count: this.state.count + 1});
-  }
+//   incrementCount = () => {
+//     this.setState({count: this.state.count + 1});
+//   }
 
-  render () {
-    return <div>
-      <h1>
-        {this.state.count}
-      </h1>
-      <button onClick={this.incrementCount}>Increment</button>
+//   render () {
+//     return <div>
+//       <h1>
+//         {this.state.count}
+//       </h1>
+//       <button onClick={this.incrementCount}>Increment</button>
+//     </div>
+//   }
+// }
+
+// Lifecycle Events -->
+function MyComponent() {
+  useEffect(() => {
+    // This will run only on mounting of the component
+    console.log("Component mounted!");
+
+    // this return fn will only runs when the component unmounts or the depedency array changes!
+    return () => {
+      console.error("Component unmounted!");
+    }
+  }, []);
+
+  return (
+    <div>
+      Inside the MyComponent Render!
     </div>
-  }
+  )
 }
 
-export default MyComponent
+export default App
