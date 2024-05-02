9ec884bc037fbbafaa3f2e0fa424bb39df0cee07
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-05-02 15:30:08 +0530

    Some imp configurations

--- week 9.2/tsc-intro/src/a.ts
+++ week 9.2/tsc-intro/src/a.ts
@@ -1,28 +1,35 @@
-"use strict";
 // Assigning :any to a variable can let you change the original type of the variable
-let x = 1;
+let x: any = 1;
 x = "String";
-function greet(firstName) {
+
+function greet(firstName: string) {
     console.log(`Hello ${firstName}`);
 }
+
 // greet("Ronit");
+
+
 // Type inference -> If you are passing any arguments to a function that are number and the fn returns their sum it is obvious that 
 // the return type would be number so it depends on you whether to specify the return type of the function or not, beacuse typescript
 // would automatically knows what type of value will be returned by the function
 // But in general it is a good practice tho to specify the return type of the functions at function parameters 
-function sum(a, b) {
-    return a + b;
+function sum (a: number, b: number): number{
+    return a+b;
 }
+
 const value = sum(3, 4);
-console.log(value);
-function isLegal(age) {
-    if (age >= 18) {
+console.log(value)
+
+function isLegal(age: number): boolean{
+    if(age >= 18){
         return true;
     }
     return false;
 }
+
 // const ans = isLegal(17);
 // console.log(ans);
+
 // Not a good way but still works
 // function callback(cb: any, delay: number){
 //     setTimeout(()=>{
@@ -30,9 +37,17 @@ function isLegal(age) {
 //         console.log(val)
 //     }, delay);
 // }
-function callback(cb, delay) {
+
+// Better approach -> callback function arguments are specified in the brackets....
+function callback(cb : ()=> void, delay: number){
     setTimeout(cb, delay);
 }
-callback(() => {
+
+callback(()=>{
     console.log("Hello world after 2s");
 }, 2000);
+
+
+// Another better approach to initialize a typescript project 
+// All the ts files that needs to be compiled are stored in a src folder and output js files to be stoed in another folder call it
+// dist or build -> that has to be configured in the tsconfig.json file in rootDir and the outDir
