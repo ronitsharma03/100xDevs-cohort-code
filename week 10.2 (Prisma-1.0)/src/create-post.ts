9466b4f7fa35752fb46e17d4b8b4d77a413a7e39
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-05-13 17:33:51 +0530

    Prisma query to insert posts for user

---
+++ week 10.2 (Prisma-1.0)/src/create-post.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main(){
+    await prisma.post.create({
+        data: {
+            title: "Last year at college",
+            content: "Hey this is my new post. In this post i will be sharing the days of my college oin last year.",
+            published: true,
+            author: {
+                connect: {
+                    id: 1
+                }
+            }
+        }
+    })
+    console.log("Created successfully!");
+}
+
+main()
+.then(async function(){
+    console.log("Disconnecting....");
+    await prisma.$disconnect();
+})
+.catch(async function(e){
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+})
