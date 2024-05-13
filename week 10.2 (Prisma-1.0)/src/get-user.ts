5ceb52ea098d6118f40d9fc47c74f5edd06e6b17
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-05-13 17:43:42 +0530

    Adding constraints to the find user query

---
+++ week 10.2 (Prisma-1.0)/src/get-user.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main(){
+    const users = await prisma.user.findMany({});
+    console.log(users);
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id: 1
+        },
+        include: {
+            posts: false
+        }
+    });
+    console.log(user);
+}
+
+
+main()
+.then(async function(){
+    await prisma.$disconnect();
+})
+.catch(async function(e) {
+    console.log(e);
+    process.exit(1);
+})
