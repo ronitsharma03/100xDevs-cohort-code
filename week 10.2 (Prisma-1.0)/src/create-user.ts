c0b9031ccb69fcd190b2ac7b3f6ab33b3cb400ee
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-05-13 17:39:54 +0530

    Prisma query to creatMany users

--- week 10.2 (Prisma-1.0)/src/create-user.ts
+++ week 10.2 (Prisma-1.0)/src/create-user.ts
@@ -3,20 +3,34 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  // ... you will write your Prisma Client queries here
-  console.log("Hi there!");
-  await prisma.user.create({
-    data: {
-        email: "ronit@gmail.com",
-        name: "Ronit"
-    }
-  });
+    // ... you will write your Prisma Client queries here
+    console.log("Hi there!");
+    await prisma.user.createMany({
+        data: [
+            {
+                email: "niman@gmail.com",
+                name: "Niman"
+            },
+            {
+                email: "anurag@gmail.com",
+                name: "Anurag"
+            },
+            {
+                email: "prashant@gmail.com",
+                name: "Prashant"
+            },
+            {
+                email: "akrati@gmail.com",
+                name: "Akrati"
+            }
+        ]
+    });
 
 }
 
 main()
-.then(async function() {
-await prisma.$disconnect();
+    .then(async function () {
+        await prisma.$disconnect();
     }).catch(async (e) => {
         console.error(e);
         await prisma.$disconnect();
