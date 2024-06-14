fcb3673427ef96684dbb0f1f3c9d2711dae47e0f
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-14 23:37:39 +0530

    TS Advanced API's

--- week12.2/src/index.ts
+++ week12.2/src/index.ts
@@ -20,5 +20,22 @@ const updateDetails = (user: newUser) => {
 const updateBody = (user: partialProfile) => {
     //body;
 }
+
+// Readonly
+interface Config {
+    endPoint: string;
+    apiKey: string;
+};
+
+const configuration: Readonly<Config> = {
+    endPoint : "https://www.google.com",
+    apiKey : "kjwnhf430934joi"
+};
+
+// configuration.endPoint = ""  // this will throw an error
+
+
+
+
 // const totalAge = sumOfAge({name:"Ronit", age: 21}, {name: "Jiro", age: 22});
 // console.log(totalAge);
