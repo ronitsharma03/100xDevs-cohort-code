4b07b6534a94beb5add3c1a57c7c865d9c63ab75
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-14 23:27:32 +0530

    TS Advanced API's

--- week12.2/src/index.ts
+++ week12.2/src/index.ts
@@ -9,5 +9,16 @@ function sumOfAge(user1: User, user2: User): number{
     return user1.age + user2.age;
 }
 
+
+type newUser = Pick<User, 'name' | 'password'>;
+type partialProfile = Partial<newUser>;
+
+const updateDetails = (user: newUser) => {
+// body;
+}
+
+const updateBody = (user: partialProfile) => {
+    //body;
+}
 // const totalAge = sumOfAge({name:"Ronit", age: 21}, {name: "Jiro", age: 22});
 // console.log(totalAge);
