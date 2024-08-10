a016277c87129f3654333473e40b62e8aa22085e
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-08-10 15:33:25 +0530

    React, backend, next app added

--- week-16-monorepos/apps/backend/src/index.ts
+++ week-16-monorepos/apps/backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import { VALUE } from "@repo/common/config";
 
 const app = express();
 
-app.get("/",  (req, res) => {
-    res.send("Hello get route!");
+app.get("/", (req, res) => {
+  console.log(VALUE);
+  res.send(VALUE);
 });
 
 app.listen(3002, () => {
-    console.log(`Server running on 3002..`);
+  console.log(`Server running on 3002..`);
 });
---
+++ week-16-monorepos/packages/common/src/index.ts
@@ -0,0 +1,2 @@
+export const VALUE = "MonoRepo & TURBOREPO";
+
