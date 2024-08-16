1f04fabe904afc11e1c126e796ae88489807ca88
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-08-16 18:21:21 +0530

    NextAuth providers github&google

--- week-16-nextAuth/nextauth/app/components/Appbar.tsx
+++ week-16-nextAuth/nextauth/app/components/Appbar.tsx
@@ -1,9 +1,8 @@
 'use client';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signIn, signOut } from 'next-auth/react';
 import React from 'react';
 
 const Appbar = () => {
-    const session = useSession();
   return (
     <div className='flex gap-2'>
         <button className='bg-slate-200 text-black p-2 rounded-md' onClick={() => {
@@ -12,7 +11,6 @@ const Appbar = () => {
         <button className='bg-slate-200 text-black p-2 rounded-md' onClick={() => {
             signOut();
         }}>Sign out</button>
-    {JSON.stringify(session)}   
     </div>
   )
 }
