51f016014f71092d6a54057a60b4af260031cb55
Ronit <ronitkhajuria03@gmail.com>
2025-03-05 19:07:37 +0530

    WebRTC components

--- week-23.1-webRTC/frontend/src/components/Sender.tsx
+++ week-23.1-webRTC/frontend/src/components/Sender.tsx
@@ -17,62 +17,64 @@ export const Sender = () => {
   }, []);
 
   const initConnection = async () => {
-    if(!socket){
-        alert("N socket found");
-        return;
+    if (!socket) {
+      alert("No socket found");
+      return;
     }
 
     socket.onmessage = async (event) => {
-        const message = JSON.parse(event.data);
-        if(message.type === 'createAnswer'){
-            await pc?.setRemoteDescription(message.sdp);
-        }else if(message.type === 'iceCandidate'){
-            pc?.addIceCandidate(message.candidate);
-        }
-    }
+      const message = JSON.parse(event.data);
+      if (message.type === "createAnswer") {
+        await pc?.setRemoteDescription(message.sdp);
+      } else if (message.type === "iceCandidate") {
+        pc?.addIceCandidate(message.candidate);
+      }
+    };
 
     const pc = new RTCPeerConnection();
     setPc(pc);
     pc.onicecandidate = (event) => {
-        if(event.candidate){
-            socket?.send(JSON.stringify({
-                type: 'iceCandidate',
-                candidate: event.candidate
-            }));
-        }
-    }
+      if (event.candidate) {
+        socket?.send(
+          JSON.stringify({
+            type: "iceCandidate",
+            candidate: event.candidate,
+          })
+        );
+      }
+    };
 
     pc.onnegotiationneeded = async () => {
-        const offer = await pc.createOffer();
-        await pc.setLocalDescription(offer);
-        socket?.send(JSON.stringify({
-            type: 'createOffer',
-            sdp: pc.localDescription
-        }));
-    }
+      const offer = await pc.createOffer();
+      await pc.setLocalDescription(offer);
+      socket?.send(
+        JSON.stringify({
+          type: "createOffer",
+          sdp: pc.localDescription,
+        })
+      );
+    };
 
     getCameraStreamAndSend(pc);
-  }
+  };
 
   const getCameraStreamAndSend = (pc: RTCPeerConnection) => {
-    navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
-        const video = document.createElement('video');
-        video.srcObject = stream;
-        video.play();
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      const video = document.createElement("video");
+      video.srcObject = stream;
+      video.play();
 
-        document.body.appendChild(video);
-        stream.getTracks().forEach((track) => {
-            pc?.addTrack(track);
-        })
+      document.body.appendChild(video);
+      stream.getTracks().forEach((track) => {
+        pc?.addTrack(track);
+      });
     });
-  }
+  };
 
   return (
     <div>
-        <div>
-            Sender
-        </div>
-        <button onClick={initConnection}>Call</button>
+      <div>Sender</div>
+      <button onClick={initConnection}>Call</button>
     </div>
-  )
+  );
 };
